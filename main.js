9756373b55f442456cb5227f98b3a6f857df318b
volumio <Volumio>
2018-11-08 17:13:17 +0100

    Working UI Webview

---
+++ main.js
@@ -0,0 +1,58 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const mdns = require('mdns');
+const path = require('path');
+
+let mainWindow
+let splash
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+      titleBarStyle: 'hidden',
+      width: 1920,
+      height: 1080,
+      show: false,
+      webPreferences: {
+          nodeIntegration: false,
+      },
+      icon: path.join(__dirname, 'assets/icons/volumio-icon.png')
+  });
+  splash = new BrowserWindow({width: 800, height: 400, transparent: true, frame: false, alwaysOnTop: true, icon: path.join(__dirname, 'assets/icons/volumio-icon.png')});
+  mainWindow.setMenu(null);
+  splash.loadURL(`file://${__dirname}/assets/splash.html`);
+  startMDNSBrowser();
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+function startMDNSBrowser(){
+    const mdnsBrowser = mdns.createBrowser(mdns.tcp('Volumio'));
+    mdnsBrowser.start();
+    mdnsBrowser.on('serviceUp', function(service) {
+
+        for (var i in service.addresses) {
+            if (service.addresses[i].length < 20) {
+                var endpoint = 'http://' + service.addresses[i] + '/';
+                mainWindow.loadURL(endpoint);
+                splash.destroy();
+                mainWindow.show();
+            }
+        }
+
+    });
+
+    mdnsBrowser.on('error', function(err) {
+        //console.log(err);
+    });
+}
